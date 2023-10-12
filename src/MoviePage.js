e02ef4ba27d7dd58c1dd71336a90f39d07d40372
Ilona Morhun <ilona@Ilonas-MacBook-Pro.local>
2023-10-12 10:33:46 -0400

    Styled MoviesPage and added rentedoptin

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
+import './styles.css';
 
 function MoviePage({ topMovies }) {
     const [searchQuery, setSearchQuery] = useState('');
     const [filteredMovies, setFilteredMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
     const [customerId, setCustomerId] = useState('');
-    const [showRentButton, setShowRentButton] = useState(false); // State to control the visibility of the "Rent Movie" button
+    const [showRentButton, setShowRentButton] = useState(false); // "Rent Movie" button
 
     const handleSearch = async () => {
         try {
@@ -18,21 +19,18 @@ function MoviePage({ topMovies }) {
         }
     };
 
-    const handleRentMovie = async () => {
+    const handleRentMovie = async (movie) => {
+        console.log('Rent Movie button clicked');
         if (!selectedMovie || !customerId) {
-            // Validation: Ensure a movie and customer are selected
             return;
         }
-
         try {
-            // Send a POST request to rent the selected movie
             const response = await Axios.post('http://localhost:3001/rentMovie', {
                 movieId: selectedMovie.film_id,
                 customerId: customerId,
             });
 
             if (response.data.success) {
-                // Show a success message or update the UI as needed
                 alert('Movie rented successfully!');
             }
         } catch (error) {
@@ -40,49 +38,67 @@ function MoviePage({ topMovies }) {
         }
     };
 
-    // Function to handle selecting a movie
-    const handleSelectMovie = (movie) => {
-        setSelectedMovie(movie);
-        setShowRentButton(true); // Show the "Rent Movie" button when a movie is selected
-    };
 
+    const handleSelectMovie = async (movie) => {
+        try {
+            const response = await Axios.get(`http://localhost:3001/getMovieDetails/${movie.film_id}`);
+            const detailedMovie = response.data;
+
+            setSelectedMovie(detailedMovie);
+            setShowRentButton(true); // Show the "Rent Movie" button
+        } catch (error) {
+            console.error("Error fetching movie details:", error);
+        }
+    };
     return (
         <div className="container">
             <div className="text-center my-4">
-                <h1 className="text-dark mb-4">Movie Page</h1>
-                <div className="d-flex justify-content-center" style={{ marginBottom: '50px' }}>
-                    <Link to="/" className="btn btn-warning mx-3" style={{ fontSize: '24px', padding: '15px 50px' }}>
-                        Home
+                <h1 className="colorful-title">Movie Page</h1>
+                <div className="button-container">
+                    <Link to="/" className="styled-button">
+                        <button>Home</button>
                     </Link>
-                    <Link to="/customers" className="btn btn-success mx-3" style={{ fontSize: '24px', padding: '15px 50px' }}>
-                        Customers
+                    <Link to="/customers" className="styled-button">
+                        <button>Customers</button>
                     </Link>
-                    <Link to="/report" className="btn btn-info mx-3" style={{ fontSize: '24px', padding: '15px 50px' }}>
-                        Report
+                    <Link to="/report" className="styled-button">
+                        <button>Report</button>
                     </Link>
                 </div>
             </div>
-
+    
             <div className="d-flex flex-column align-items-center mt-5">
-                <input
-                    type="text"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    placeholder="Search by film name, actor, or genre..."
-                    className="form-control form-control-lg mb-3"
-                    style={{ width: '80%' }}
-                />
-                <button onClick={handleSearch} className="btn btn-primary btn-lg">
-                    Search
-                </button>
-
-                
-
-                {/* Render the "Rent Movie" button when a movie is selected */}
-                {showRentButton && (
-                    <button onClick={handleRentMovie} className="btn btn-success btn-lg mt-3">
-                        Rent Movie
+                <div className="search-container"> {/* Create a container for search */}
+                    <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        placeholder="Search by film name, actor, or genre..."
+                        className="form-control form-control-lg mb-3"
+                        style={{ width: '80%' }}
+                    />
+                    <button onClick={handleSearch} className="btn btn-primary btn-lg">
+                        Search
                     </button>
+                </div>
+
+                {selectedMovie && (
+                    <div>
+                        <p><strong>Description:</strong> {selectedMovie.description}</p>
+                        <p><strong>Rating:</strong> {selectedMovie.rating}</p>
+                        {selectedMovie.actors && (
+                            <p><strong>Actors:</strong> {selectedMovie.actors.join(', ')}</p>
+                        )}
+                        <p><strong>Rental Rate:</strong> {selectedMovie.rental_rate}</p>
+                        <p><strong>Rented:</strong> {selectedMovie.rental_count} times</p>
+
+                        {/* "Rent Movie" button associated with the selected movie */}
+                        {showRentButton && (
+                            <button onClick={() => handleRentMovie(selectedMovie)} className="btn btn-success btn-lg mt-3">
+                                Rent Movie
+                            </button>
+                        )}
+                    </div>
                 )}
             </div>
 
@@ -90,18 +106,11 @@ function MoviePage({ topMovies }) {
                 {filteredMovies.map((movie) => (
                     <li key={movie.film_id}>
                         <span
-                            // Add an onClick event to select the movie
                             onClick={() => handleSelectMovie(movie)}
                             style={{ cursor: 'pointer', textDecoration: 'underline' }}
                         >
                             {movie.title}
                         </span>
-                        {selectedMovie === movie && (
-                            <div>
-                                <p><strong>Description:</strong> {selectedMovie.description}</p>
-                                <p><strong>Rented:</strong> {movie.rental_count} times</p>
-                            </div>
-                        )}
                     </li>
                 ))}
             </ul>
